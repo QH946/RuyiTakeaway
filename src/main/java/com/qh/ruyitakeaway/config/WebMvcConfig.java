a1d9cb1e8de10a98f78e798bc0d9c619acd6f9a3
Quorra <923158428@qq.com>
2022-09-07 19:32:26 +0800

    :tada:初始化项目

---
+++ src/main/java/com/qh/ruyitakeaway/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.qh.ruyitakeaway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @Author QH
+ * @Date 2022/9/7 19:29
+ * @Package: com.qh.ruyitakeaway.config
+ * @Version 1.0
+ */
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始静态资源映射");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
